7d32afa4343734ab72b130f3a0799f64cc131eaf
Salwa-Mansour <alqadasisal@gmail.com>
2021-07-11 11:02:24 +0300

    lesson done

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const banner = document.querySelector('#page-banner');
-console.log()
+console.log('#page-banner node type is :',banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('page banner chiileds : ',banner.hasChildNodes());
+
+const clonedBanner = banner.cloneNode(false);
+console.log(clonedBanner)
