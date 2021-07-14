031200be0faf1be0930ccf86367d7faca5f5e3f0
Salwa-Mansour <alqadasisal@gmail.com>
2021-07-14 09:52:47 +0300

    lesson done

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-var btns = document.querySelectorAll('#book-list .delete');
-
-Array.from(btns).forEach(function(btn){
-    btn.addEventListener('click',function(e){
-    //    console.log(e)
-        var li = e.target.parentElement;
-      // console.log(li)
-     li.parentNode.removeChild(li);
-    });
+//delete book
+const list = document.querySelector('#book-list ul');
+list.addEventListener('click',function(e){
+    if(e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        // li.parentNode.removeChild(li);
+        list.removeChild(li);
+        // console.log('aha')
+    };
 });
-
-const link = document.querySelector('#page-banner a');
-link.addEventListener('click',function(e){
-    e.preventDefault();
-    console.log('navagation to ',e.target.textContent ,'was preverneted');
-})
